9b7e03dd888df190beda8bb40ac1a60c3af35525
RIFAT-VAI143 <144621265+RIFAT-VAI143@users.noreply.github.com>
2025-08-27 18:16:20 +0600

    Create RIFATXZ.py

---
+++ RIFATXZ.py
@@ -0,0 +1,21 @@
+#!/data/data/com.termux/files/usr/bin/python3.12
+# -*- coding: utf-8 -*-
+import os
+import subprocess
+ 
+bit = os.uname().machine
+changes = subprocess.getoutput("git status --porcelain")
+ 
+if changes:
+    os.system("git reset --hard")
+    os.system("git clean -fd")
+    os.system("git pull")
+ 
+os.system("chmod 777 *")
+ 
+if '64' in bit:
+    import rifatxz
+else:
+    os.system("clear")
+    print("TOOL NOT AVAILABLE FOR 32 BIT DEVICE")
+ 
